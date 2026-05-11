27300b06dc83b94e665718f1134e147955d53d47
Ibrahim <ibx1232@gmail.com>
2026-05-11 15:33:37 +0200

    Add VM IP to ALLOWED_HOSTS

--- truck_signs_designs/settings/base.py
+++ truck_signs_designs/settings/base.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(ROOT_BASE_DIR,'templates')
 
 
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '46.224.26.224',]
 
 
 # Application definition
